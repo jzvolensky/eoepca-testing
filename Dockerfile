beb08502c0ca4dbef98fd4c098cab5aaac6249c7
¨jzvolensky¨ <juro.zvolensky@gmail.com>
2023-10-03 14:59:57 +0000

    ades 0.1

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,17 @@
+FROM ubuntu:22.04
+
+WORKDIR /app
+
+COPY ./scripts /app/scripts
+
+RUN apt-get update && apt-get install -y python3 python3-pip -y git
+
+COPY requirements.txt /app
+
+RUN pip install -r requirements.txt
+
+
+
+
 
 
--- scripts/processing/ades.py
+++ scripts/processing/ades.py
@@ -6,63 +6,90 @@ import sys
 import requests
 import boto3
 import yaml
+import pprint
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
+pp = pprint.PrettyPrinter(indent=4)
+# Load config file
+# If custom_config.txt is not present in root
+# Use default_config.yaml in scripts/config/
+def Load_Config():
+    '''
+    Function to load config file
+    If custom config is not present in the root directory
+    uses the default values (from deployment guide)
 
-# Assuming verify_ades_config.yaml is in the same directory as the script
-config_file_path = os.path.join(current_dir, "ades_default_config.yaml")
+    '''
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Now you can open the config file
-with open(config_file_path, 'r') as stream:
+    config_path = os.path.join(current_dir, '..', 'config', 'default_config.yaml')
+
+    
+
+    print('LOADING CONFIG')
+    if os.path.isfile('custom_config.yaml'):
+        config_file = open('custom_config.yaml', 'r')
+        config = yaml.safe_load(config_file)
+        print(f'CUSTOM CONFIG LOADED: {config}')
+        config_file.close()
+    else:
+        config_file = open(config_path, 'r')
+        config = yaml.safe_load(config_file)
+        print('DEFAULT CONFIG LOADED')
+        pp.pprint(config)
+        config_file.close()
+
+    base_domain = config['base_domain']
+    base_url = config['base_url']
+    platform_domain = config['platform_domain']
+    user_name = config['USER_NAME']
+    user_password = config['USER_PASSWORD']
+    
+    return config, base_domain, base_url, platform_domain, user_name, user_password
+
+def Setup_Urls(base_domain, user_name):
+
+    ades_base_url = "http://ades-open." + base_domain
+    ades_wps_url = ades_base_url + "/" + user_name + "/zoo"; print("ADES WPS endpoint:", ades_wps_url)
+    ades_proc_url = ades_base_url + "/" + "_run/wps3"; print("ADES API Processes endpoint:", ades_proc_url)
+    print(ades_wps_url)
+    print(ades_proc_url)
+    print("ADES urls retrieved")
+
+    return ades_proc_url, ades_wps_url
+
+
+def List_Processes(ades_proc_url):
+    '''
+    Function to make API calls to ades to get list of avilable processes
+    '''
     try:
-        config = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
+        response = requests.get(ades_proc_url)
+        print(f'1. URL from the List processes functions: {ades_proc_url}')
+        if response.status_code == 200:
+            print("ADES API call successful")
+            response_json = response.json()
+            print("ADES processes retrieved:")
+            pp.pprint(response_json)
+        else:
+            print("ADES API call failed")
+    except requests.exceptions.RequestException as e:
+        print(e)
+    
+    try:
+        response2 = requests.get(ades_proc_url + "/processes")
+        print(f'2. URL from the List processes functions: {ades_proc_url}')
+        if response2.status_code == 200:
+            print("ADES API call successful")
+            response_json = response2.json()
+            print("ADES processes retrieved:")
+            pp.pprint(response_json)
+        else:
+            print("ADES API call failed")
+    except requests.exceptions.RequestException as e:
+        print(e)
 
-base_domain = config['base_domain']
-platform_domain = config['platform_domain']
-base_url = config['base_url']
-USER_NAME = config['USER_NAME']
-USER_PASSWORD = config['USER_PASSWORD']
-S3_ENDPOINT = config['S3_ENDPOINT']
-
-print(f"Configuration loaded successfuly, \n Base domain: {base_domain}, \n Base url: {base_url}")
-
-from utils import DemoClient as client
-
-demo = client.DemoClient(base_url)
-demo.register_client()
-demo.save_state()
-print("demo client registered")
-
-
-# ADES service URLs
-ades_base_url = "http://ades-open." + base_domain
-ades_wps_url = ades_base_url + "/" + USER_NAME + "/zoo"; print("ADES WPS endpoint:", ades_wps_url)
-ades_proc_url = ades_base_url + "/" + USER_NAME + "/wps3"; print("ADES API Processes endpoint:", ades_proc_url)
-print(ades_wps_url)
-print(ades_proc_url)
-print("ADES urls retrieved")
-
-ades_access_token = None
-app_name = "s-expression-0_0_2"
-
-print(f"Using app name {app_name} , and access token {ades_access_token}")
-
-# Testing requests to the ADES server using the requests library
-
-url = ades_proc_url + "/processes"
-print(url)
-headers= {
-    'authorization': f'Bearer {ades_access_token}',
-    'Accept': 'application/json',
-}
-
-response = requests.get(url, headers=headers, verify=False)
-
-if response.status_code == 200:
-    # Successful response, parse and work with the JSON data if needed
-    data = response.json()
-    print(data)
-else:
-    print(f"Request failed with status code {response.status_code}")
+if __name__ == '__main__':
+    
+    config, base_domain, base_url, platform_domain, user_name, user_password = Load_Config()
+    ades_proc_url, ades_wps_url = Setup_Urls(base_domain, user_name)
+    List_Processes(ades_proc_url=ades_proc_url)
